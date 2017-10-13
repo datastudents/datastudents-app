daba4c4961d78c3d7590f7a731b573a0af0ac8c3
Arian <arianmarkus77@yahoo.com>
2017-10-13 17:13:19 +0700

    student data half

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,42 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 const studentCollection = [
   {
     id: 0,
     name: "Marulam",
     gender: "Male",
-    education:"SD",
-    age:43
+    education: "SD",
+    age: 43
   },
   {
     id: 1,
     name: "Oi",
     gender: "Female",
-    education:"SMU",
-    age:15
-  },
-]
+    education: "SMU",
+    age: 15
+  }
+];
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  next()
-})
+  );
+  next();
+});
+
+//push
+const addItemToCollection = (collection, item) => {
+  collection.push(item);
+  return collection; // updated collection
+};
 
 app.get("/", (req, res) => {
-  res.send(studentCollection)
-})
+  res.send(studentCollection);
+});
 
 app.listen(3000, () => {
-  console.log("API running on localhost:3000")
-})
+  console.log("API running on localhost:3000");
+});
